c4643ac2b6e165fd20b04389f9aafabf6529b44d
Robin <me@mempler.de>
2021-04-24 22:55:03 +0200

    Update Platform.hh

--- Components/Config/include/Kokoro/Config/Platform.hh
+++ Components/Config/include/Kokoro/Config/Platform.hh
@@ -2,16 +2,24 @@
 
 #ifdef __linux__
     #define KOKORO_LINUX 1
+#else
+    #define KOKORO_LINUX 0
 #endif
 
 #ifdef _WIN32
     #define KOKORO_WIN32 1
+#else
+    #define KOKORO_WIN32 0
 #endif
 
 #ifdef __EMSCRIPTEN__
     #define KOKORO_EMSCRIPTEN 1
+#else
+    #define KOKORO_EMSCRIPTEN 0
 #endif
 
 #ifdef __ANDROID__
     #define KOKORO_ANDROID 1
+#else
+    #define KOKORO_ANDROID 0
 #endif
